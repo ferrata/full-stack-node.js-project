b07c5c8e95f7d0cd231480b04040c7538fdd96bf
Sasha Goloshchapov <ferrata@hey.com>
2023-03-25 12:27:22 -0500

    Added event cards

--- front-end/events-app/src/components/Events/Events.tsx
+++ front-end/events-app/src/components/Events/Events.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { EventCard } from "../EventCard";
 
 export const Events = () => {
-  const isLoggedIn = localStorage.getItem("token") ? true : false;
+  const [events, setEvents] = useState([]);
+  const token = localStorage.getItem("token");
+  const isLoggedIn = token ? true : false;
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
+
+    fetch("http://localhost:5000/events", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+      });
+  }, [isLoggedIn, token]);
 
   if (!isLoggedIn) {
     return <Navigate to="/login" />;
   }
 
-  return <h1>Events</h1>;
+  return (
+    <>
+      <h1>Events</h1>
+      {events.map((event: any) => (
+        <EventCard key={event.id} event={event} />
+      ))}
+    </>
+  );
 };
