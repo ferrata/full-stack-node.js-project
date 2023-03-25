517d1dbd1c4fc1f0c86d5182f382dd1659ed6c3c
Sasha Goloshchapov <ferrata@hey.com>
2023-03-25 12:25:53 -0500

    backend: Removed a duplicated /event route; renamed /events-with-participants to /event/:id and now it returns a single event with participants

--- back-end/src/admin-login/accountsAuth.js
+++ back-end/src/admin-login/accountsAuth.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import { jwt_Secret, MYSQL_CONFIG } from "../config.js";
+import { jwt_Secret } from "../config.js";
 import jwt from "jsonwebtoken";
-import mysql from "mysql2/promise";
 
 const router = Router();
 
@@ -15,18 +14,5 @@ export const isLoggedIn = (req, res, next) => {
     return res.status(400).send(`Incorrect Token`).end();
   }
 };
-
-router.get("/events", isLoggedIn, async (req, res) => {
-  try {
-    const con = await mysql.createConnection(MYSQL_CONFIG);
-    const [data] = await con.execute(`SELECT * FROM events`);
-
-    await con.end();
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-    return res.status(401).send({ message: "You are not in our base" }).end();
-  }
-});
 
 export default router;
