8679b3cba3bcd3a7bec6a75362050d7207c243cf
Sasha Goloshchapov <ferrata@hey.com>
2023-03-25 01:59:42 -0500

    Added index route to /events. Events component now checks if user us loggedIn

--- front-end/events-app/src/App.tsx
+++ front-end/events-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Events } from "./components/Events";
 import { LoginForm } from "./components/LoginForm/LoginForm";
 import { NotFoundPage } from "./components/NotFoundPage";
@@ -7,6 +7,7 @@ export const App = () => {
   return (
     <>
       <Routes>
+        <Route index element={<Navigate to="/events" />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/events" element={<Events />} />
         <Route path="/*" element={<NotFoundPage />} />
--- front-end/events-app/src/components/Events/Events.tsx
+++ front-end/events-app/src/components/Events/Events.tsx
@@ -1,3 +1,11 @@
+import { Navigate } from "react-router-dom";
+
 export const Events = () => {
+  const isLoggedIn = localStorage.getItem("token") ? true : false;
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return <h1>Events</h1>;
 };
